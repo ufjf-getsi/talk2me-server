5e0ff66a1e634cdbfa9f5fa9726c2184edae7567
Mateus Gonçalo <mateusgon57@gmail.com>
2019-09-15 18:40:12 -0300

    Commit inicial

---
+++ src/main/java/br/ufjf/getsi/talktwome/Talk2Me.java
@@ -0,0 +1,14 @@
+package br.ufjf.getsi.talktwome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Talk2Me {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Talk2Me.class, args);
+		System.out.println("Queijo");
+	}
+
+}
