8b1652e91b68a1fdb00c9040574e1f3d35dcf0a4
Mateus Gonçalo <mateusgon57@gmail.com>
2019-09-17 21:39:04 -0300

    Refatorando IndexController

--- src/main/java/br/ufjf/getsi/talktwome/Controllers/IndexController.java
+++ src/main/java/br/ufjf/getsi/talktwome/Controllers/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndex()
